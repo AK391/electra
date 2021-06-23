8a58b3197cd66dc914993d9c4ad11893dd79c554
AK391 <81195143+AK391@users.noreply.github.com>
2021-06-23 01:52:40 -0400

    add examples and update desc

--- gradio/demo.py
+++ gradio/demo.py
@@ -1,3 +1,9 @@
 import gradio as gr
 
-gr.Interface.load("huggingface/google/electra-small-generator").launch()
+description = "demo for Electra. To use it, simply add your text, or click one of the examples to load them. Read more at the links below."
+article = "<p style='text-align: center'><a href='https://arxiv.org/abs/2003.10555'>ELECTRA: Pre-training Text Encoders as Discriminators Rather Than Generators</a> | <a href='https://github.com/google-research/electra'>Github Repo</a></p>"
+
+
+
+gr.Interface.load("huggingface/google/electra-small-generator", description=description, article=article, examples=[
+["Paris is the [MASK] of France."]] ).launch()
